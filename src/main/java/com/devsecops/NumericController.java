e89efff942a5ee956da826d80fe53a0e044a9e17
clagosu <clagosu@users.noreply.github.com>
2024-04-16 21:40:17 -0400

    se agrega validacion

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -33,6 +33,7 @@ public class NumericController {
 		@GetMapping("/compare/{value}")
 		public String compareToFifty(@PathVariable int value) {
 			String message = "Could not determine comparison";
+			String validate = "validacion";
 			if (value > 50) {
 				message = "Greater than 50";
 			} else {
